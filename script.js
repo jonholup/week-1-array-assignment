dd85aa85131f701cdf177b34c413a4997c816356
Jonny Holupchinski <jonholup@gmail.com>
2017-02-08 21:06:49 -0600

    tested empty strings as arguments

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 function bigVsSmall(a, b) {
-  a = parseFloat(a);
+  console.log('Before: ', a, b);
+  a = parseFloat(a);  //
   b = parseFloat(b);
-  if (a > b) {
+  console.log('After: ', a, b);
+  if (isNaN(a) || isNaN(b)) {
+    alert('Input numbers first!');
+    return;
+  } else if (a > b) {
     return 'The first number was bigger!';
   } else if (a < b) {
     return 'The second number was bigger!';
-  } else if (a === b) {
+  } else {
     return 'The numbers are the same!';
   }
-  }
+}
 
 console.log(bigVsSmall(2, 4));
 console.log(bigVsSmall(4, 2));
 console.log(bigVsSmall(3, 3));
 console.log(bigVsSmall('86.7 taco', 92));
+console.log(bigVsSmall('taco 86.7', 92));
+console.log(bigVsSmall('', ''));
